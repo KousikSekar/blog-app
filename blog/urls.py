f0644a0c1c511becb2444f8b882749adf66a822f
Kousik <kousik.sekar@gmail.com>
2020-12-01 10:55:53 +0530

    Added feature to list the user specific posts

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     # path('', views.home, name='blog-home'),
+    path('user/<str:username>', views.UserPostListView.as_view(), name='user-posts'),
     path('', views.PostListView.as_view(), name='blog-home'),
     path('post/<int:pk>', views.PostDetailView.as_view(), name='post-detail'),
     path('post/new', views.PostCreateView.as_view(), name='post-create'),
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http.response import HttpResponse
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from . import models
+from django.contrib.auth.models import User
 
 posts = [
     {
@@ -33,7 +34,20 @@ class PostListView(ListView):
     template_name = 'blog/home.html'
     context_object_name = 'posts'
     ordering = ['-date_posted']
-    
+    paginate_by = 3
+
+
+class UserPostListView(ListView):
+    model = models.Post
+    template_name = 'blog/user_post.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+    paginate_by = 3
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return models.Post.objects.filter(author=user).order_by('-date_posted')
+        
 
 class PostDetailView(DetailView):
     model = models.Post
@@ -46,6 +60,7 @@ class PostCreateView(LoginRequiredMixin, CreateView):
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
+
 
 class PostUpdateView(UserPassesTestMixin, LoginRequiredMixin, UpdateView):
     model = models.Post
@@ -60,6 +75,7 @@ class PostUpdateView(UserPassesTestMixin, LoginRequiredMixin, UpdateView):
         if self.request.user == post.author:
             return True
         return False
+
 
 class PostDeleteView(UserPassesTestMixin, LoginRequiredMixin, DeleteView):
     model = models.Post
